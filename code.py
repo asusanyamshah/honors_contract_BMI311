b40231988f12e646ff0a944de5cd6d7e5d0a0a1d
asusanyamshah <121066354+asusanyamshah@users.noreply.github.com>
2024-09-14 17:58:12 -0700

    Deleted code.py

--- code.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import confusion_matrix, accuracy_score
-from imblearn.over_sampling import RandomOverSampler
-import os
